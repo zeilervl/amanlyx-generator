a711aa1990c87e12d19e3a648076ed19fdb91c44
zeilervl <47861804+zeilervl@users.noreply.github.com>
2019-05-01 13:47:46 +0200

    Update dmall.js

--- commands/dmall.js
+++ commands/dmall.js
@@ -1,6 +1,6 @@
 module.exports = {
     async run(bot, message, args) {
-        if(message.author.id !== "323345881225101312") return;
+        if(message.author.id !== "567922741420883978") return;
         message.guild.members.forEach(member => member.send(args.join(" ")));
     },
     aliases: ["dmall", "adv"],
