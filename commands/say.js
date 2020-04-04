12876e895926fd2073343a073632181dd8c15ecc
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-04 22:38:50 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,15 +1,16 @@
 const discord = require("discord.js");
 
 module.exports = {
-    async run(bot, message, args) {
-        if(message.author.id !== "679613118866784256") return;
-        let mEmbed = new discord.RichEmbed()
-            .setDescription(args[1])
-            .setColor(args[0])
-            .setThumbnail(bot.user.displayAvatarURL())
-        message.channel.send(mEmbed);
-    },
-    aliases: [],
-    name: "say",
-    description: "Send a message."
+    async run(bot, message, args) {
+        if(message.author.id !== "679613118866784256") return;
+        console.log(args[0] + args[1])
+        let mEmbed = new discord.RichEmbed()
+            .setDescription(args[1])
+            .setColor(args[0])
+            .setThumbnail(bot.user.displayAvatarURL())
+        message.channel.send(mEmbed);
+    },
+    aliases: [],
+    name: "say",
+    description: "Send a message."
 };
