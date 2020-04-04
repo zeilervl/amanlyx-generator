c375b27219197bcbeca39f591dee4ba9ee00ce3a
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-04 19:34:30 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports = {
     async run(bot, message, args) {
         if(message.author.id !== "679613118866784256") return;
-		bot.user.setPresence({ activity: { type: args[0].toUpperCase(), name: message.slice(args[0].length, message.length))} })
+bot.user.setPresence({ activity: { type: args[0].toUpperCase(), name: message.slice(args[0].length, message.length)})
     },
     aliases: [],
     name: "changestatus",
