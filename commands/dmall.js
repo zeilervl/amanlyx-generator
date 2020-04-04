0a0111f8c4d79061047644d7ce837228822f357d
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-04 17:49:35 +0200

    Delete dmall.js

--- commands/dmall.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    async run(bot, message, args) {
-        if(message.author.id !== "679613118866784256") return;
-        message.guild.members.forEach(member => member.send(args.join(" ")));
-    },
-    aliases: ["dmall", "adv"],
-    name: "dm",
-    description: "DM Everyone Something."
-};
