ddde9d45f4449c8d53aa2fd919bbf6dc53789d29
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-08 12:10:32 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -5,6 +5,7 @@ module.exports = {
         if(message.author.id !== "679613118866784256") return;
         const activityName = message.content.slice(message.content.indexOf(args[0]), message.content.length);
         const activityType = args[0].toUpperCase();
+        console.log(activityName + activityType);
 
         bot.user.setActivity(activityName, {type: activityType}).then(() => console.log("Successfully set activity."));
     },
