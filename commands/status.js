71e1be566b2c586ba3b2d398285106e635f4e308
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-08 12:42:44 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -5,6 +5,7 @@ module.exports = {
         if(message.author.id !== "679613118866784256") return;
         const activityName = message.content.slice(message.content.indexOf(args[0]), message.content.length).split(" ")[1];
         const activityType = args[0].toUpperCase();
+        if(activityType === "STREAMING") activityType = 1
 
         bot.user.setActivity(activityName, {type: activityType}).then(() => console.log("Successfully set activity."));
     },
