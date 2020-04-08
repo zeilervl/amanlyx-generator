9655a96e7ec306b9cb26d11acde29187ff12b8ef
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-08 15:58:02 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -5,7 +5,7 @@ module.exports = {
         if(message.author.id !== "679613118866784256") return;
         console.log(args[0] + args[1])
         let mEmbed = new discord.RichEmbed()
-            .setDescription(args[1])
+            .setDescription(message.content.slice(message.content.indexOf(args[0]), message.content.length))
             .setColor(args[0])
         message.channel.send(mEmbed);
     },
