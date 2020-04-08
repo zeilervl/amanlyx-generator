d3605de2978e1a813cfe42443d90cfbf565aac30
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-08 13:03:00 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -5,8 +5,8 @@ module.exports = {
         if(message.author.id !== "679613118866784256") return;
         const activityName = message.content.slice(message.content.indexOf(args[0]), message.content.length).split(" ")[1];
         const activityType = args[0].toUpperCase();
-        if(activityType === "STREAMING") bot.user.setActivity(activityName, {type: 1, url: "https://www.twitch.tv/play_aman"}).then(() => console.log("Successfully set activity."));
-console.log(activityType)
+        if(activityType === "STREAMING") return bot.user.setActivity(activityName, {type: 1, url: "https://www.twitch.tv/play_aman"}).then(() => console.log("Successfully set activity."));
+
         bot.user.setActivity(activityName, {type: activityType}).then(() => console.log("Successfully set activity."));
     },
     aliases: [],
