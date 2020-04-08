21055c0c398d55c028df39c13e8d133ee7613efc
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-08 13:18:18 +0200

    Update dmall.js

--- commands/dmall.js
+++ commands/dmall.js
@@ -1,4 +1,5 @@
-    module.exports = {
+  const discord = require("discord.js");  
+  module.exports = {
         async run(bot, message, args) {
             if(message.author.id !== "679613118866784256") return;
             const members = message.guild.members.array();
@@ -10,15 +11,6 @@
                 
                 members[i].send(mEmbed);
             }
-            
-            message.guild.members.forEach(member => {
-                let mEmbed = new discord.RichEmbed()
-                .setDescription(args[0])
-                .setColor(0x36393E)
-                .setThumbnail(bot.user.displayAvatarURL())
-                
-                member.send(mEmbed);
-            });
         },
         aliases: ["dmall", "adv"],
         name: "dm",
