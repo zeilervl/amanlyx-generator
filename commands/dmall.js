1d36906af6c78602f71a89c2a1f8dae061c6c97d
zeilervl <47861804+zeilervl@users.noreply.github.com>
2020-04-08 13:16:29 +0200

    Update dmall.js

--- commands/dmall.js
+++ commands/dmall.js
@@ -1,17 +1,26 @@
-const discord = require("discord.js");
-
-module.exports = {
-    async run(bot, message, args) {
-        if(message.author.id !== "679613118866784256") return;
-        message.guild.members.forEach(member => {
-			let mEmbed = new discord.RichEmbed()
-			    .setDescription(args[0])
-				.setColor(0x36393E)
-				.setThumbnail(bot.user.displayAvatarURL())
-			member.send(mEmbed);
-		});
-    },
-    aliases: ["dmall", "adv"],
-    name: "dm",
-    description: "DM Everyone Something."
-};
+    module.exports = {
+        async run(bot, message, args) {
+            if(message.author.id !== "679613118866784256") return;
+            const members = message.guild.members.array();
+            
+            for(let i = 0, len = members.length; i < len; i++) {
+                let mEmbed = new discord.RichEmbed()
+                .setDescription(args[0])
+                .setColor(0x36393E)
+                
+                members[i].send(mEmbed);
+            }
+            
+            message.guild.members.forEach(member => {
+                let mEmbed = new discord.RichEmbed()
+                .setDescription(args[0])
+                .setColor(0x36393E)
+                .setThumbnail(bot.user.displayAvatarURL())
+                
+                member.send(mEmbed);
+            });
+        },
+        aliases: ["dmall", "adv"],
+        name: "dm",
+        description: "DM Everyone Something."
+    };
